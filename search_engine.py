e935c05af65675afbf8570d75f0d78a4bdbb1f17
Danial Kord <danial.kordmodanlou@gmail.com>
2021-05-25 16:36:32 +0430

    [feat] added normalizer and system for removing high frequency tokens for optimizational purpose

--- search_engine.py
+++ search_engine.py
@@ -1,10 +1,14 @@
-
 from tokenizer import Tokenizer
+import pandas as pd
+import copy
 
+min_high_frequency_tokens = 0.8
 
-tokens = set
+tokens = []
 
-inverted_index = dict
+inverted_index = {}
+
+ignorance_tokens =[]
 
 def update_inverted_index_list(token,ID):
     if inverted_index.keys().__contains__(token) is False:
@@ -16,22 +20,41 @@ def update_inverted_index_list(token,ID):
         for index in range(len(inverted_token_indexes)):
             if ID > inverted_token_indexes[index]:
                 place += 1
-            elif ID == inverted_token_indexes[index]:
+            elif ID < inverted_token_indexes[index]:
+                break
+            else:
                 return
         inverted_index[token].insert(place,ID)
 
 
-def add_new_file(path,file_ID):
-    with open(path, "r") as txt_file:
-        lines = txt_file.readlines()
-        for line in lines:
-            # statement = "S " + i
-            current_tokens = Tokenizer.get_tokens(line)
-            for token in current_tokens:
-                tokens.add(token)
-                update_inverted_index_list(token,file_ID)
+def add_new_file(path,content_ID_col_name,content_col_name):
+    data = pd.read_excel(path)
+    for index, row in data.iterrows():
+        ID = row[content_ID_col_name]
+        line = row[content_col_name]
+        # statement = "S " + i
+        print(ID)
+        current_tokens = Tokenizer.get_tokens(line)
+        for token in current_tokens:
+            if token in ignorance_tokens:
+                continue
+            normalized_token = Tokenizer.token_normalizer(token)
+            if not normalized_token in tokens:
+                tokens.append(normalized_token)
+            update_inverted_index_list(normalized_token,ID)
 
 
+
+
+
+
+def clear_most_repeated_tokens():
+    copy_tokens = copy.deepcopy(tokens)
+    for token in copy_tokens:
+        if len(inverted_index[token]) >= len(copy_tokens) * min_high_frequency_tokens:
+            tokens.remove(token)
+            inverted_index.pop(token)
+    copy_tokens.clear()
 
 
 
@@ -43,6 +66,16 @@ def main():
     out = Tokenizer.get_tokens(test)
 
     for i in out:
+        print(i)
+
+    test = "بدترین"
+    print(Tokenizer.token_normalizer(test))
+    input()
+
+    add_new_file("C:\Danial\Projects\Danial\Information_Retrieval_Search_Engine\IR_Spring2021_ph12_7k.xlsx","id","content")
+    clear_most_repeated_tokens()
+
+    for i in tokens:
         print(i)
 
 
--- tokenizer.py
+++ tokenizer.py
@@ -2,7 +2,13 @@
 import re
 
 
-split_characters = "[?., !#${}() % + = \\\ \\- _ * ` ~ @ ^ /]+"
+split_characters = "[?., : ; !#${}() % + = \\\ \\- _ * ` ~ @ ^ / ، \n]+"
+
+end_token_redundant = ['تر',
+                    'ترین'
+                    ,'ها'
+                    ,'اً'
+                    ]
 
 class Tokenizer:
 
@@ -12,3 +18,16 @@ class Tokenizer:
 
     def get_tokens(text):
         return re.split(split_characters,text)
+
+    def get_normalized_tokens(text):
+        tokens = Tokenizer.get_tokens(text)
+        for index in range(len(tokens)):
+            tokens[index] = Tokenizer.token_normalizer(tokens[index])
+        return tokens
+
+    def token_normalizer(token):
+        for i in end_token_redundant:
+            if token.endswith(i):
+                token = token[0:len(token) - len(i)]
+                break
+        return token
